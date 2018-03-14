61657e018f2393cd4f58fe832d6eca075c0ede7a
James Skett <jamesskett31@gmail.com>
2018-03-14 12:19:51 +0000

    added monster class

---
+++ FMP/Monster.cpp
@@ -0,0 +1,16 @@
+#include "Monster.h"
+
+
+
+Monster::Monster(XMFLOAT4 colour, float x, float y, float z, float scale, float width, float height) :
+	Entity(colour, x, y, z, scale, width, height)
+{
+
+}
+
+
+Monster::~Monster()
+{
+
+}
+
---
+++ FMP/Monster.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Entity.h"
+class Monster : public Entity
+{
+public:
+	Monster(XMFLOAT4 colour, float x, float y, float z, float scale, float width, float height);
+	~Monster();
+};
+
