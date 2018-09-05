ba9ce84aa07ecc8c3e0bf4dc6779f6a7b6edfb74
James Skett <jamesskett31@gmail.com>
2018-09-05 16:05:20 +0100

    added camera class so new perspectives can be used

---
+++ FMP/Camera.cpp
@@ -0,0 +1,247 @@
+#include "Camera.h"
+#include <windows.h>
+#include <stdio.h>
+
+#include <math.h>
+
+Camera::Camera(float x, float y, float z, float rotation)
+{
+	m_x = x;
+	m_y = y;
+	m_z = z;
+	m_camera_rotation = rotation;
+
+	m_dx = sinf(XMConvertToRadians(m_camera_rotation));
+	m_dz = cosf(XMConvertToRadians(m_camera_rotation));
+	m_dy = sinf(XMConvertToRadians(m_camera_rotation));
+
+	m_isForward = true;
+	m_isBackward = false;
+	m_isRight = false;
+	m_isLeft = false;
+}
+
+Camera::~Camera()
+{
+}
+
+void Camera::Rotate(float number_of_degrees, float objectX, float objectY, float objectZ)
+{
+	/*m_camera_rotation += number_of_degrees;
+
+	m_dx = sinf(XMConvertToRadians(m_camera_rotation));
+	m_dy = sinf(XMConvertToRadians(m_camera_rotation));
+	m_dz = cosf(XMConvertToRadians(m_camera_rotation));*/
+
+	float s = sinf(XMConvertToRadians(number_of_degrees));
+	float c = cosf(XMConvertToRadians(number_of_degrees));
+
+	m_x -= objectX;
+	m_y -= objectZ;
+
+	float xnew = (m_x * c) - (m_y * s);
+	float znew = (m_y * s) + (m_x * c);
+
+	m_x = znew;
+	m_y = xnew;
+}
+
+void Camera::Forward(float distance)
+{
+	float oldX = m_x;
+	float oldZ = m_z;
+
+	m_x += m_dx * distance;
+	m_z += m_dz * distance;
+}
+
+void Camera::Up(float distance)
+{
+	m_y += distance;
+}
+
+void Camera::Strafe(float distance)
+{
+	//move the camera left and right based on the direction
+	m_position += (m_right * distance);
+
+	//update the cameras coordinates
+	m_x = XMVectorGetX(m_position);
+	m_y = XMVectorGetY(m_position);
+	m_z = XMVectorGetZ(m_position);
+
+
+}
+
+void Camera::Pitch(float number_of_degrees)
+{
+	m_camera_rotation += number_of_degrees;
+
+	m_dx = sinf(XMConvertToRadians(m_camera_rotation));
+	m_dy = sinf(XMConvertToRadians(m_camera_rotation));
+	m_dz = cosf(XMConvertToRadians(m_camera_rotation));
+	
+	m_up = XMVector3Cross(m_right, m_lookat);
+
+}
+
+void Camera::SetX(float x)
+{
+	m_x = x;
+}
+
+void Camera::SetY(float y)
+{
+	m_y = y;
+}
+
+void Camera::SetZ(float z)
+{
+	m_z = z;
+}
+
+void Camera::SetYaw(float angle)
+{
+	m_camYaw = angle;
+}
+
+void Camera::SetPitch(float angle)
+{
+	m_camPitch = angle;
+}
+
+void Camera::SetIsForward(bool isForward)
+{
+	m_isForward = isForward;
+}
+
+void Camera::SetIsBackward(bool isBackward)
+{
+	m_isBackward = isBackward;
+}
+
+void Camera::SetIsRight(bool isRight)
+{
+	m_isRight = isRight;
+}
+
+void Camera::SetIsLeft(bool isLeft)
+{
+	m_isLeft = isLeft;
+}
+
+float Camera::GetX()
+{
+	return m_x;
+}
+
+float Camera::GetY()
+{
+	return m_y;
+}
+
+float Camera::GetZ()
+{
+	return m_z;
+}
+
+float Camera::GetDirectionX()
+{
+	return m_dx;
+}
+
+float Camera::GetDirectionY()
+{
+	return m_dy;
+}
+
+float Camera::GetDirectionZ()
+{
+	return m_dz;
+}
+
+// makes the camera follow the position of an object with a slight offset
+void Camera::CameraFollow(float targetX, float targetY, float targetZ)
+{
+	if (m_isForward) //if isForward is true the camera faces down the z axis positively
+	{
+		m_x = targetX;
+		m_y = targetY + 3;
+		m_z = targetZ - 15;
+
+		m_dx = sinf(XMConvertToRadians(0.0f));
+		m_dz = cosf(XMConvertToRadians(0.0f));
+	}
+	else if(m_isBackward) //if isBackward is true the camera faces down the z axis negatively
+	{
+		m_x = targetX;
+		m_y = targetY + 3;
+		m_z = targetZ + 15;
+
+		m_dx = sinf(XMConvertToRadians(180.0f));
+		m_dz = cosf(XMConvertToRadians(180.0f));
+	}
+	else if (m_isRight) //if isRight is true the camera faces down the x axis negatively
+	{
+		m_x = targetX + 15;
+		m_y = targetY + 3;
+		m_z = targetZ;
+
+		m_dx = sinf(XMConvertToRadians(270.0f));
+		m_dz = cosf(XMConvertToRadians(270.0f));
+	}
+	else if (m_isLeft) //if isLeft is true the camera faces down the y axy positively
+	{
+		m_x = targetX - 15;
+		m_y = targetY + 3;
+		m_z = targetZ;
+
+		m_dx = sinf(XMConvertToRadians(90.0f));
+		m_dz = cosf(XMConvertToRadians(90.0f));
+	}
+
+	
+}
+
+void Camera::LookAt(float targetX, float targetZ)
+{
+	m_camera_rotation = atan2f((targetX - m_x), (targetZ - m_z)) * (180 / XM_PI);
+
+	m_dx = sinf(XMConvertToRadians(m_camera_rotation));
+	m_dz = cosf(XMConvertToRadians(m_camera_rotation));
+}
+
+
+XMMATRIX Camera::GetViewMatrix()
+{
+	/*m_camRotationMatrix = XMMatrixRotationRollPitchYaw(m_camPitch, m_camYaw, 0);
+	m_lookat = XMVector3TransformCoord(m_defaultForward, m_camRotationMatrix);
+	m_lookat = XMVector3Normalize(m_lookat);*/
+
+	//XMMATRIX rotateYTempMatrix;
+	//rotateYTempMatrix = XMMatrixRotationY(m_camYaw);
+
+	//m_right = XMVector3TransformCoord(m_defaultRight, rotateYTempMatrix);
+	//m_up = XMVector3TransformCoord(m_up, rotateYTempMatrix);
+	//m_forward = XMVector3TransformCoord(m_defaultForward, rotateYTempMatrix);
+
+	/*m_position += m_moveLeftRight * m_right;
+	m_position += m_moveBackForward * m_forward;
+
+	m_moveLeftRight = 0.0f;
+	m_moveBackForward = 0.0f;
+
+	m_lookat = m_position + m_lookat;*/
+
+
+	m_position = XMVectorSet(m_x, m_y, m_z, 0.0f);
+	m_lookat = XMVectorSet(m_x + m_dx, m_y + m_dy, m_z + m_dz, 0.0f);
+	m_up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	
+	//Get the forward and right vector positions
+	m_forward = XMVector3Normalize(m_lookat - m_position);
+	m_right = XMVector3Cross(m_forward, m_up);
+
+	return XMMatrixLookAtLH(m_position, m_lookat, m_up);
+}
+
---
+++ FMP/Camera.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <dxerr.h>
+
+#include <DirectXMath.h>
+using namespace DirectX;
+
+__declspec(align(16)) class Camera
+{
+public:
+	void* operator new(size_t i)
+	{
+		return _mm_malloc(i, 16);
+	}
+
+	void operator delete(void* p)
+	{
+		return _mm_free(p);
+	}
+
+	Camera(float x, float y, float z, float rotation);
+	~Camera();
+
+	//Functions to manipulate the camera
+	void Rotate(float number_of_degrees, float objectX, float objectY, float objectZ);
+	void Forward(float distance);
+	void Up(float distance);
+	void Strafe(float distance);
+	void Pitch(float number_of_degrees);
+
+	//setters
+	void SetX(float x);
+	void SetY(float y);
+	void SetZ(float z);
+	void SetYaw(float angle);
+	void SetPitch(float angle);
+	void SetIsForward(bool isForward);
+	void SetIsBackward(bool isBackward);
+	void SetIsRight(bool isRight);
+	void SetIsLeft(bool isLeft);
+
+	//get the x,y,z positions of the camera
+	float GetX();
+	float GetY();
+	float GetZ();
+	float GetDirectionX();
+	float GetDirectionY();
+	float GetDirectionZ();
+
+	void CameraFollow(float targetX, float targetY, float targetZ);
+
+	void LookAt(float targetX, float targetZ);
+
+	XMMATRIX GetViewMatrix();
+
+private:
+	float m_x, m_y, m_z;
+	float m_dx, m_dy, m_dz;
+	float m_camera_rotation;
+	float m_camPitch = 0.0f;
+	float m_camYaw = 0.0f;
+	float m_moveLeftRight = 0.0f;
+	float m_moveBackForward = 0.0f;
+
+	XMVECTOR m_position = XMVectorSet(0.0f, 0.0f, -0.5f, 0.0f);
+	XMVECTOR m_lookat;
+	XMVECTOR m_up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMVECTOR m_defaultForward = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
+	XMVECTOR m_defaultRight = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
+	XMVECTOR m_forward = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
+	XMVECTOR m_right = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
+
+	XMMATRIX m_camRotationMatrix;
+	XMMATRIX m_groundWorld;
+
+	//Used for changing direction camera is facing
+	bool m_isForward, m_isBackward, m_isRight, m_isLeft;
+};
+
